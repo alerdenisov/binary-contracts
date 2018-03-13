19846632993f75804ad57ff5c6b024052ffe0560
Aler Denisov <aler.zampillo@gmail.com>
2018-03-13 18:56:33 +0300

    contract code

---
+++ contracts/Greetener.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.4.18;
+
+contract Hello {
+  // Save address of latest greeter called greet function
+  address public lastestGreeter;
+
+  // Greet event fires when someone calls greet function
+  event Greet(address indexed _who);
+  
+  // Greet with everyone inside ethereum blockchain
+  function greet() public returns (bool) {
+    lastestGreeter = msg.sender;
+    Greet(lastestGreeter);
+    return true;
+  }
+}
