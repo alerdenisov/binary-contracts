cfaa118b9eb0233719e11679c9f825fd4bfa7a70
Aler Denisov <aler.zampillo@gmail.com>
2018-03-15 19:21:13 +0300

    counter comment remote to readability

--- contracts/examples/counter/ICounter.sol
+++ contracts/examples/counter/ICounter.sol
@@ -20,28 +20,9 @@
 
 pragma solidity ^0.4.18;
 
-/// @title Base interface of all counter implementation
-/// @author Aler Denisov
 interface ICounter {
-  /// @notice Method to increase the counter
-  /// @param _storage Instance of uint storage of counter
-  /// @dev Should be overrided by children contracts
   function increaseCounter(address _storage) public returns (uint);
-
-  /// @notice Method to receive current value of the counter
-  /// @param _storage Instance of uint storage of counter
-  /// @return Current value of counter 
-  /// @dev Should be overrided by children contracts
   function getCounter(address _storage) public view returns (uint);
-
-  /// @notice Method to validate received storage
-  /// @param _storage Instance of uint storage of counter
-  /// @return True if storage is valid, false\revert overwise
   function validateStorage(address _storage) public view returns (bool);
-
-  /// @notice Method to transfer ownership of storage to another counter
-  /// @param _storage Instance of uint storage of counter
-  /// @param _counter Instance of ICounter implementation to transfer ownership
-  /// @return True if ownership is transfered, false\revert overwise
   function transferStorage(address _storage, address _counter) public returns (bool);
 }
