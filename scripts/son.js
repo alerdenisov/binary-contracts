039d4e3d2fde301f6c3dfb74d8730a736115b30a
Aler Denisov <aler.zampillo@gmail.com>
2018-03-15 18:14:48 +0300

    show bloom log

--- scripts/son.js
+++ scripts/son.js
@@ -4,5 +4,6 @@ module.exports = async function () {
   const son = await Son.new()
   const tx = await son.goToHome()
 
+  console.log(tx.receipt.logsBloom)
   console.log(tx.logs.map(log => log.event).join('\n'))
 }
